79aba0b776f7a5fa036aa50c395f2550daa04a2e
Rahmannugar <cladeadenugar@gmail.com>
2023-02-24 18:05:56 +0100

    added

---
+++ README.md
@@ -0,0 +1,9 @@
+## University backend via PHP.
+
+This is a server side system to handle CRUD operations.
+This can be used with any JS framework and Vanilla JS also.
+
+File Tree.
+DBConnect - To connect your xampp/wampp server to your database.
+.htaccess - To allow your sever read through your files.
+Index.php - This contains the entire CRUD operation code. Can be tweaked to tend to your inputs.
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+echo "Testing";
+
+include "DbConnect.php";
+$objDb = new DbConnect;
+$conn = $objDb->connect();
+
+$method = $_SERVER["REQUEST_METHOD"];
+switch($method) {
+    case "GET":
+        $sql = "SELECT * FROM users";
+        $path = explode("/", $_SERVER["REQUEST_URL"]);
+        if(isset($path[3]) && is_numeric($path[3])){
+            $sql .= " WHERE id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(":id", $path[3]);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_decode($users);
+    break;
+    case "POST":
+        $user = json_decode(file_get_contents("php://input"));
+        $sql = "INSERT INTO users(id, firstName, lastName, birthDate, gender, country, zipCode, course, email, phone, password) VALUES(null, :firstName, :lastName, :birthDate, :gender, :country, :zipCode, :course, :email, :phone, :password)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(":firstName", $user->firstName);
+        $stmt->bindParam(":lastName", $user->lastName);
+        $stmt->bindParam(":birthDate", $user->birthDate);
+        $stmt->bindParam(":gender", $user->gender);
+        $stmt->bindParam(":country", $user->country);
+        $stmt->bindParam(":zipCode", $user->zipCode);
+        $stmt->bindParam(":course", $user->course);
+        $stmt->bindParam(":email", $user->email);
+        $stmt->bindParam(":phone", $user->phone);
+        $stmt->bindParam(":password", $user->password);
+        if($stmt->execute()) {
+            $response = ["status" => 1, "message" => "Record created successfully"];
+        } else {
+              $response = ["status" => 0, "message" => "Failed to create record."];
+        }
+        echo json_encode($response);
+        break;
+        
+    case "PUT":
+        $user = json_decode( file_get_contents('php://input') );
+        $sql = "UPDATE users SET firstName= :firstNname, lastName =:lastName, birthDate =:birthDate, gender =:gender, country =:country, zipCode =:zipCode, course =:course, email =:email, phone =:phone, WHERE id = :id";
+        $stmt = $conn->prepare($sql);
+       $stmt->bindParam(":firstName", $user->firstName);
+        $stmt->bindParam(":lastName", $user->lastName);
+        $stmt->bindParam(":birthDate", $user->birthDate);
+        $stmt->bindParam(":gender", $user->gender);
+        $stmt->bindParam(":country", $user->country);
+        $stmt->bindParam(":zipCode", $user->zipCode);
+        $stmt->bindParam(":course", $user->course);
+        $stmt->bindParam(":email", $user->email);
+        $stmt->bindParam(":phone", $user->phone);
+        $stmt->bindParam(":password", $user->password);
+        if($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record updated successfully.'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to update record.'];
+        }
+        echo json_encode($response);
+        break;
+
+    case "DELETE":
+        $sql = "DELETE FROM users WHERE id = :id";
+        $path = explode('/', $_SERVER['REQUEST_URI']);
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id', $path[3]);
+        
+}
